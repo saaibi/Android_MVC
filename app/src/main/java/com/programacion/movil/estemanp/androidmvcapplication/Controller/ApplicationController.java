79d45c52baa91000cfc4637eb7838759dfc5b240
estemanp <estemanp@MacBook-Pro-de-Mac-estemanp.local>
2016-09-12 20:56:36 -0500

    Commit: Ejemplo de base de datos con SQLiteOpenHelper

--- app/src/main/java/com/programacion/movil/estemanp/androidmvcapplication/Controller/ApplicationController.java
+++ app/src/main/java/com/programacion/movil/estemanp/androidmvcapplication/Controller/ApplicationController.java
@@ -1,9 +1,18 @@
 package com.programacion.movil.estemanp.androidmvcapplication.Controller;
 
 import android.app.Application;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.AsyncTask;
+import android.widget.Toast;
 
 import com.programacion.movil.estemanp.androidmvcapplication.Domain.User;
+import com.programacion.movil.estemanp.androidmvcapplication.Repository.UserContract;
+import com.programacion.movil.estemanp.androidmvcapplication.Repository.UsersDbHelper;
+import com.programacion.movil.estemanp.androidmvcapplication.View.LandingActivity;
 
+import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,10 +22,11 @@ import java.util.List;
 public class ApplicationController extends Application {
 
     private List<User> users;
+    private UsersDbHelper usersDbHelper;
 
     public  ApplicationController(){
         users=new ArrayList<User>();
-        fillDateBases();
+        //fillDateBases();
     }
 
     private void fillDateBases(){
@@ -32,7 +42,7 @@ public class ApplicationController extends Application {
         users.add(person2);
     }
 
-    public boolean isValidUser(String username, String password){
+    private boolean isValidUser(String username, String password){
         Boolean resp=false;
         for (User person:users) {
             if(username.equals(person.getUserName())){
@@ -42,5 +52,51 @@ public class ApplicationController extends Application {
             }
         }
         return resp;
+    }
+
+
+    public void authenticate(String username, String password){
+        usersDbHelper = new UsersDbHelper(this.getApplicationContext());
+        new AuthenticTask().execute(username,password);
+    }
+
+    private class AuthenticTask extends AsyncTask<String, Void, Boolean> {
+
+        @Override
+        protected Boolean doInBackground(String... params) {
+            List<User> listUsers= new ArrayList<>();
+            Cursor cursor=usersDbHelper.getAllUsers();
+            cursor.moveToFirst();
+            User userAux;
+            while(!cursor.isAfterLast()) {
+                /*userAux=new User(cursor.getString(cursor.getColumnIndex(UserContract.UserEntry.NAME)),
+                        cursor.getString(cursor.getColumnIndex(UserContract.UserEntry.LAST_NAME)),
+                        cursor.getString(cursor.getColumnIndex(UserContract.UserEntry.USER_NAME)),
+                        cursor.getString(cursor.getColumnIndex(UserContract.UserEntry.PASSWORD)),
+                        cursor.getInt(cursor.getColumnIndex(UserContract.UserEntry.AGE)));*/
+                userAux=new User(cursor);
+                listUsers.add(userAux);
+                cursor.moveToNext();
+            }
+            System.out.println("Termino de traer los datos");
+            users=listUsers;
+
+            return isValidUser(params[0],params[1]);
+        }
+
+        @Override
+        protected void onPostExecute(Boolean isAuthenticate) {
+            super.onPostExecute(isAuthenticate);
+            if(isAuthenticate){
+                Intent intent = new Intent();
+                intent.setClass(getApplicationContext(), LandingActivity.class);
+                intent.setAction(LandingActivity.class.getName());
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
+
+                getApplicationContext().startActivity(intent);
+            }else{
+                Toast.makeText(getApplicationContext(), "El usuario y contraseña no coinciden", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
--- app/src/main/java/com/programacion/movil/estemanp/androidmvcapplication/View/LoginActivity.java
+++ app/src/main/java/com/programacion/movil/estemanp/androidmvcapplication/View/LoginActivity.java
@@ -29,11 +29,6 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     public void login(View view) {
-        if(appController.isValidUser(userName.getText().toString(),password.getText().toString())) {
-            Intent intent = new Intent(this, LandingActivity.class);
-            startActivity(intent);
-        }else{
-            Toast.makeText(this, "El usuario y contraseña no coinciden", Toast.LENGTH_SHORT).show();
-        }
+        appController.authenticate(userName.getText().toString(),password.getText().toString());
     }
 }
