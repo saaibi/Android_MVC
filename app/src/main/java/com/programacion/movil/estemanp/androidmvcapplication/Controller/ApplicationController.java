03aecd09b8de894a3f105f799c86fc31203191ab
saaibi yesid florez <saaibiyesid@hotmail.com>
2017-09-04 19:49:49 -0500

    Update ApplicationController.java

--- app/src/main/java/com/programacion/movil/estemanp/androidmvcapplication/Controller/ApplicationController.java
+++ app/src/main/java/com/programacion/movil/estemanp/androidmvcapplication/Controller/ApplicationController.java
@@ -1,32 +1,27 @@
 package com.programacion.movil.estemanp.androidmvcapplication.Controller;
 
-import android.app.Application;
-import android.content.Context;
-import android.content.Intent;
-import android.database.Cursor;
-import android.os.AsyncTask;
-import android.widget.Toast;
-
 import com.programacion.movil.estemanp.androidmvcapplication.Domain.User;
-import com.programacion.movil.estemanp.androidmvcapplication.Repository.UserContract;
-import com.programacion.movil.estemanp.androidmvcapplication.Repository.UsersDbHelper;
-import com.programacion.movil.estemanp.androidmvcapplication.View.LandingActivity;
 
-import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by estemanp on 19/08/16.
  */
-public class ApplicationController extends Application {
+public class ApplicationController  {
 
     private List<User> users;
-    private UsersDbHelper usersDbHelper;
 
+    private  static  ApplicationController _instance;
+    public static ApplicationController geInstance(){
+        if (_instance == null){
+            _instance = new ApplicationController();
+        }
+        return _instance;
+    }
     public  ApplicationController(){
-        users=new ArrayList<User>();
-        //fillDateBases();
+        users= new ArrayList<>();
+        fillDateBases();
     }
 
     private void fillDateBases(){
@@ -35,14 +30,14 @@ public class ApplicationController extends Application {
     }
 
     private void fillUsers(){
-        User person1=new User("Andres","Perez","estemanp","1234",24);
+        User person1=new User("Saaibi","Florez","saaibi","1234",24);
         users.add(person1);
 
         User person2=new User("Jessica","Ramirez","jessica","1234",16);
         users.add(person2);
     }
 
-    private boolean isValidUser(String username, String password){
+    public boolean isValidUser(String username, String password){
         Boolean resp=false;
         for (User person:users) {
             if(username.equals(person.getUserName())){
@@ -52,51 +47,5 @@ public class ApplicationController extends Application {
             }
         }
         return resp;
-    }
-
-
-    public void authenticate(String username, String password){
-        usersDbHelper = new UsersDbHelper(this.getApplicationContext());
-        new AuthenticTask().execute(username,password);
-    }
-
-    private class AuthenticTask extends AsyncTask<String, Void, Boolean> {
-
-        @Override
-        protected Boolean doInBackground(String... params) {
-            List<User> listUsers= new ArrayList<>();
-            Cursor cursor=usersDbHelper.getAllUsers();
-            cursor.moveToFirst();
-            User userAux;
-            while(!cursor.isAfterLast()) {
-                /*userAux=new User(cursor.getString(cursor.getColumnIndex(UserContract.UserEntry.NAME)),
-                        cursor.getString(cursor.getColumnIndex(UserContract.UserEntry.LAST_NAME)),
-                        cursor.getString(cursor.getColumnIndex(UserContract.UserEntry.USER_NAME)),
-                        cursor.getString(cursor.getColumnIndex(UserContract.UserEntry.PASSWORD)),
-                        cursor.getInt(cursor.getColumnIndex(UserContract.UserEntry.AGE)));*/
-                userAux=new User(cursor);
-                listUsers.add(userAux);
-                cursor.moveToNext();
-            }
-            System.out.println("Termino de traer los datos");
-            users=listUsers;
-
-            return isValidUser(params[0],params[1]);
-        }
-
-        @Override
-        protected void onPostExecute(Boolean isAuthenticate) {
-            super.onPostExecute(isAuthenticate);
-            if(isAuthenticate){
-                Intent intent = new Intent();
-                intent.setClass(getApplicationContext(), LandingActivity.class);
-                intent.setAction(LandingActivity.class.getName());
-                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
-
-                getApplicationContext().startActivity(intent);
-            }else{
-                Toast.makeText(getApplicationContext(), "El usuario y contraseña no coinciden", Toast.LENGTH_SHORT).show();
-            }
-        }
     }
 }
